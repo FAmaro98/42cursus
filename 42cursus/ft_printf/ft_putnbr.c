3b0f73fb2a09be99b6f676c51c373739819a072d
FAmaro98 <123091587+FAmaro98@users.noreply.github.com>
2023-04-20 17:15:41 +0100

    Add files via upload

---
+++ 42cursus/ft_printf/ft_putnbr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: franalme <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/19 18:54:20 by franalme          #+#    #+#             */
+/*   Updated: 2023/04/19 18:54:20 by franalme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putnbr(int n)
+{
+	long long int	n2;
+	int				l;
+
+	l = 0;
+	n2 = n;
+	if (n2 < 0)
+	{
+		l++;
+		n2 = n2 * (-1);
+		write(1, "-", 1);
+	}
+	if (n2 >= 10)
+	{
+		l += ft_putnbr(n2 / 10);
+		l += ft_putnbr(n2 % 10);
+	}
+	if (n2 <= 9)
+		l += ft_putchar(n2 + '0');
+	return (l);
+}
+
+int	ft_putnbr_u(unsigned int n)
+{
+	unsigned int	n2;
+	int				l;
+
+	l = 0;
+	n2 = n;
+	if (n2 >= 10)
+	{
+		l += ft_putnbr_u(n2 / 10);
+		l += ft_putnbr_u(n2 % 10);
+	}
+	if (n2 <= 9)
+		l += ft_putchar(n2 + '0');
+	return (l);
+}
